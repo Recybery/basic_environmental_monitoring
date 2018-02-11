8b94554d0b88a9e3d0cb7b7e7ea718871703a48d
csoeder <charles.soeder@gmail.com>
2018-02-10 20:01:26 -0500

    accepts arbitrary serial port as argument

--- command_line_serial_reader.sh
+++ command_line_serial_reader.sh
@@ -3,4 +3,4 @@ cereal=$1
 #e.g., /dev/ttyACM1
 phial_out=$2
 
-ttylog -b 9600 -d /dev/ttyACM1 -f |  ts '%Y-%m-%d	%H:%M:%S' | tee "$phial_out"
+ttylog -b 9600 -d "$cereal" -f |  ts '%Y-%m-%d	%H:%M:%S' | tee "$phial_out"
